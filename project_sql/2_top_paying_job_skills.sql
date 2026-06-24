28fbbe64b3f4c17626445e0d72de53965a6a4887
viny <vinayakmishra6102004@gmail.com>
2026-06-24 22:55:18 +0530

    top skills query 2

---
+++ project_sql/2_top_paying_job_skills.sql
@@ -0,0 +1,25 @@
+-- Question: what skills are required for the top paying data analyst jobs?
+-- :use the top 10 highest paying data analyst jobs from the first query 
+WITH top_paying_jobs AS(
+SELECT 
+    job_id,
+    job_title,
+    job_location,
+    salary_year_avg,
+    name AS company_name
+FROM
+    job_postings_fact
+    LEFT JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
+WHERE
+    job_title_short = 'Data Analyst' AND
+    job_location = 'Anywhere' AND
+    salary_year_avg IS NOT NULL
+ORDER BY salary_year_avg DESC
+LIMIT 10
+)
+SELECT top_paying_jobs.*,
+skills_dim.skills
+FROM top_paying_jobs 
+inner JOIN skills_job_dim ON top_paying_jobs.job_id = skills_job_dim.job_id
+inner JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+ORDER BY salary_year_avg DESC
